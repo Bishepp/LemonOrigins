23456dd031c9a434d1709ed198c5ac361b7cd363
Bishepp <116900828+Bishepp@users.noreply.github.com>
2025-03-06 14:52:28 +0300

    v1.0.0-beta

---
+++ common/src/main/java/ru/lemoncraft/lemonorigins/Constants.java
@@ -0,0 +1,11 @@
+package ru.lemoncraft.lemonorigins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MODID = "lemonorigins";
+	public static final String MOD_NAME = "LemonOrigins";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
